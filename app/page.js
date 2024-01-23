48aed74f53dd457f8569160bfc3bd69aefb6285c
fantingsheng <fants0230@gmail.com>
2024-01-23 18:53:46 +0800

    地图页面完善

--- app/page.js
+++ app/page.js
@@ -1,47 +1,83 @@
+'use client';
 import Image from "next/image";
 import { Button } from '@radix-ui/themes';
+import { useEffect } from 'react';
+
+function MapContainer() {
+  let map = null;
+ 
+  useEffect(() => {
+    import("@amap/amap-jsapi-loader").then(AMapLoader => {
+    AMapLoader.load({
+      key: "f92d916868bf43323d76c1f76702d2a6", // 申请好的Web端开发者Key，首次调用 load 时必填
+      version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+      plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+    })
+      .then((AMap) => {
+        map = new AMap.Map("container", {
+          viewMode: "3D", // 是否为3D地图模式
+          zoom: 11, // 初始化地图级别
+          center: [120.325121, 31.498017], // 初始化地图中心点位置
+        });
+        const marker = new AMap.Marker({
+          position: new AMap.LngLat(120.325121, 31.498017),
+          content: '<div class="custom-content-marker">' +
+          '   <img src="/img/poi-marker-red.png" width="30">' +
+          '</div>',
+          offset: new AMap.Pixel(-50, -15)
+        })
+        map.add(marker);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    })
+  
+    return () => {
+      map?.destroy();
+    };
+  }, []);
+
+  return (
+    <div
+      id="container"
+      style={{ height: "100%", width: "100%" }}
+    ></div>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex bg-[#E6E4E0] sm:h-screen w-screen sm:overflow-hidden">
+      <aside className="w-full sm:w-[430px] bg-primary min-h-screen sm:overflow-y-auto sm:overflow-x-hidden sm:absolute top-0 bottom-0 px-5">
+        <div className='pt-3'>
+          <nav className='flex justify-end'>
+            <Button>添加</Button>
+          </nav>
+          <header className='py-16 text-center'>
+            <h3>足迹</h3>
+          </header>
+          <section>
+            <div className='sticky top-0 z-10 flex justify-between px-5 py-4 -mx-5 bg-blur'>
+              <h1 className='text-2xl font-bold text-primary'>所有线路</h1>
+            </div>
+            <ol className='pt-1'>
+              <a className='relative block p-4 mb-5 transition border rounded border-primary hover:border-primary-hover'>
+
+              </a>
+              <a className='relative block p-4 mb-5 transition border rounded border-primary hover:border-primary-hover'>
+                
+              </a>
+            </ol>
+          </section>
+          <footer className='pb-6 text-center text-secondary'>
+            @copyright 2024, by Timfan.
+          </footer>
         </div>
-      </div>
+      </aside>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      <Button>Let's go</Button>
+      <div className='block text-xl text-forest ml-[430px] h-screen relative w-full'>
+        <MapContainer />
       </div>
     </main>
   );
