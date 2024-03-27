61e3bdbb656f860a76b32b77bc4240cd8276eb74
fantingsheng <fants0230@gmail.com>
2024-03-27 18:37:49 +0800

    fix: 渲染博客文章列表

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,11 +1,20 @@
-export default function PostList ({ posts }) {
+import type { Post } from ".contentlayer/generated";
+import Link from "next/link";
+
+type PostProps = {
+  posts: Array<Post>
+};
+
+export default function PostList ({ posts }: PostProps) {
   return (
     <ul
       className="flex flex-col animated-list"
     >
       {posts.length === 0 && <p>No posts found</p>}
       {posts.map((post) => (
-        <li>{post.title}</li>
+        <li key={post._id}>
+          <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+        </li>
       ))}
     </ul>
   )
--- content/definitions/post.tsx
+++ content/definitions/post.tsx
@@ -1,5 +1,22 @@
-import { defineDocumentType } from "contentlayer/source-files";
- 
+import { defineDocumentType, ComputedFields } from "contentlayer/source-files";
+
+const getSlug = (doc: any) => doc._raw.sourceFileName.replace(/\.mdx$/, "");
+
+const postComputedFields: ComputedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => getSlug(doc),
+  },
+  image: {
+    type: "string",
+    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`,
+  },
+  og: {
+    type: "string",
+    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`,
+  },
+};
+
 export const Post = defineDocumentType(() => ({
   name: "Post",
   filePathPattern: "posts/*.mdx",
@@ -14,4 +31,5 @@ export const Post = defineDocumentType(() => ({
       required: true,
     },
   },
+  computedFields: postComputedFields
 }));
