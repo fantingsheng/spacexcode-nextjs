b32e32938a29e9e32a958a3d7434a2bcf1218934
fantingsheng <fants0230@gmail.com>
2024-04-09 17:56:02 +0800

    fix: fix padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           <Header />
           <div
             className={
-              "px-6 md:px-6 pt-16 pb-24 md:pt-20 md:pb-44 max-w-[700px] mx-auto"
+              "px-6 md:px-6 pt-16 pb-24 md:pt-20 md:pb-20 max-w-[700px] mx-auto"
             }
           >
             {children}
